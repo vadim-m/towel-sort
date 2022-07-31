f7383ae773e1fb2cc3746b5fa83e82d5509b74c0
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-07-31 21:54:54 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
+module.exports = function ttowelSort(matrix) {
+    if (matrix === undefined || matrix.length == 0) return [];
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    const newtArr = matrix.map((item, index) => {
+        if (index % 2 !== 0) return item.reverse();
+        return item;
+    });
+
+    return [].concat(...newtArr);
+};
